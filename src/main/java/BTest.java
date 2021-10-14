3006854f74dc46064e53419713f8ffacf65341a2
Andrea Fernández <andreafernez@estudiantec.cr>
2021-10-14 16:04:49 -0600

    btest
    borrador

--- src/main/java/BTest.java
+++ src/main/java/BTest.java
@@ -1,2 +1,35 @@
 public class BTest {
+
+    //public static void main(String[] args) {
+       // BTree b = new BTree (3, null);
+      //  b.insertKey(12);
+       // b.insertKey(24);
+     //   b.insertKey(37);
+        //b.insertKey(3);
+        //b.insertKey(45);
+      //  b.insertKey(50);
+    //    b.printfTree(b.getRoot(),0);
+  //  }
+
+//}
+
+// para el b-tree.java
+//public boolean insertKey(int key){
+//    Result result = searchTree(root,key);
+//    if(!result.isTag()){
+//        return insertBTree(key, result.getPtr(), result.getI());
+//    }
+//    return false;
+// public void printfTree(BSTNode root,int count){
+//		count ++;
+//		for(int i = 0;i<=root.getKeynum();i++){
+//			if(i == 0){
+//				System.out.println(root.getKeys().toString());
+//			}
+//			if(root.getChilds().get(i) != null){
+//				 System.out.print ("the" + count + "layer" + i + "children:");
+//				printfTree(root.getChilds().get(i),count);
+//			}
+//		}
+//	}
 }
