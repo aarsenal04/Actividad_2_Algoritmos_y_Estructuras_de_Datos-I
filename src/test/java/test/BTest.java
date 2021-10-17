5194e0fffb17101e9d6a6f66ca77bcc656e95a0b
Daniel Kohkemper <dkohkemper@gmail.com>
2021-10-17 16:30:50 -0600

    Deleting code from test

--- src/test/java/test/BTest.java
+++ src/test/java/test/BTest.java
@@ -15,23 +15,4 @@ public class BTest {
 
 }
 
-// para el b-tree.java
-public boolean insertKey(int key){
-    Result result = searchTree(root,key);
-    if(!result.isTag()){
-        return insertBTree(key, result.getPtr(), result.getI());
-    }
-    return false;
- public void printfTree(BSTNode root,int count){
-		count ++;
-		for(int i = 0;i<=root.getKeynum();i++){
-			if(i == 0){
-				System.out.println(root.getKeys().toString());
-			}
-			if(root.getChilds().get(i) != null){
-				 System.out.print ("the" + count + "layer" + i + "children:");
-				printfTree(root.getChilds().get(i),count);
-			}
-		}
-	}
 }
